902d85fb0272b85e02f779b0d7c00367c2148e38
manq2010 <manq2010@gmail.com>
2023-02-22 20:46:46 +0200

    Use models to insert data

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
+
+  after_create :increment_user_posts_counter
+
+  private
+
+  def increment_user_posts_counter
+    author.increment!(:posts_counter)
+  end
 end

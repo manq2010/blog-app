87dd6193ed7be53ade5f8b2819994293736fc7c4
manq2010 <manq2010@gmail.com>
2023-02-22 14:38:59 +0200

    Update migrations and database

--- db/migrate/20230222122757_create_post.rb
+++ db/migrate/20230222122757_create_post.rb
@@ -3,8 +3,9 @@ class CreatePost < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.string :title
       t.string :text
-      t.integer :comment_counter
+      t.integer :comments_counter
       t.integer :likes_counter
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20230222123048_create_comment.rb
+++ db/migrate/20230222123048_create_comment.rb
@@ -2,6 +2,8 @@ class CreateComment < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.string :text
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
 
       t.timestamps
     end

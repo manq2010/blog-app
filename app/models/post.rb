637d8a0bc49e3bd1f46156f6db1a3f02dbb0826c
manq2010 <manq2010@gmail.com>
2023-03-01 16:37:22 +0200

    Fix validation bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,7 @@ class Post < ApplicationRecord
   after_create :increment_user_posts_counter
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :likes_counter, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :comments_counter, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   def five_most_recent_comments
     comments.order(created_at: :desc).limit(5)

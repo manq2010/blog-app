16b5753b441e0b3765a06bef385aa3d2e8e27043
manq2010 <manq2010@gmail.com>
2023-03-09 22:15:07 +0200

    Add styles to device views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,20 @@
 class ApplicationController < ActionController::Base
   layout 'standard'
 
+  before_action :authenticate_user!
+
   protect_from_forgery with: :exception
 
-  before_action :update_allowed_parameters, if: :devise_controller?
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+  # before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def update_allowed_parameters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :photo, :bio, :email, :password, :current_password)
